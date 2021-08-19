2f753f829e82793e7a786a4c06e2c167a8c9f194
psekharGitHub <purnendusekhar44@gmail.com>
2021-08-19 20:43:31 +0530

    Added Ajax, Form-handling, life-cycle, roting folders to repo

--- src/Styling/Stylesheet.js
+++ src/Styling/Stylesheet.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Styling from './Styling.css'
+import './Styling.css'
 
 function Stylesheet() {
+
+    let className = `primary-enlarge-text`
     return (
         <div>
             <h1 className="primary">StyleSheet</h1>
+            <p className={`${className}`}> Random Text is always Random </p>
+            <div className="success">
+                This is div text
+            </div>
         </div>
     )
 }
--- src/components/props/Greeting.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Greeting = () => {
-    return <div>
-        
-    </div>
-}
